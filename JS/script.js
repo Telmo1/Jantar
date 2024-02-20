162b029579d53051273d3d89a9a636eedcfcd4be
Yuri Telmo <yrtelmo00@gmail.com>
2024-02-19 21:17:30 -0300

    TC

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
 function responderConvite() {
     var dataSelecionada = document.getElementById("data").value;
-    var resposta = "Obrigada por confirmar sua presença para o jantar no dia " + dataSelecionada + ". Mal posso esperar para compartilhar esse momento incrível com você!";
+    var resposta = "Obrigado por confirmar sua presença para o jantar no dia " + dataSelecionada + ". Mal posso esperar para compartilhar esse momento incrível com você!";
     document.getElementById("resposta").innerText = resposta;
+
+    // Preenchendo o coração
+    document.getElementById("coracao").classList.add("coracao-preenchido");
 
     // Salvando a resposta no armazenamento local do navegador
     localStorage.setItem("resposta", resposta);
@@ -12,5 +15,7 @@ window.onload = function() {
     var respostaSalva = localStorage.getItem("resposta");
     if (respostaSalva) {
         document.getElementById("resposta").innerText = respostaSalva;
+        // Verificando se já foi respondido para preencher o coração
+        document.getElementById("coracao").classList.add("coracao-preenchido");
     }
 };
